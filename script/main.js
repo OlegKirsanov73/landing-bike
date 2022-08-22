3d68d9420a968cb61dc794e41368f685fb3158b0
Oleg Kirsanov <110236611+OlegKirsanov73@users.noreply.github.com>
2022-08-23 01:48:38 +0300

    Create main.js

---
+++ script/main.js
@@ -0,0 +1,9 @@
+$(function(){
+    $('.bike-slider, .slider-item').slick({
+        arrows:false,
+        dots:true,
+        fade:true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+});
